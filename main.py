9daac50e4993427b0a4dcb36bf4b5809eca3cd57
Илья <orskilya@gmail.com>
2022-02-04 19:53:21 +0500

    переключение отображения

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ class Example(Ui_MainWindow, QMainWindow):
         self.static_map_link = 'https://static-maps.yandex.ru/1.x'
         self.z = 10
         self.ll = [37.622513, 55.753220]
-        self.l_list = ['map', 'sat', 'skl']
+        self.current_visibility = 'map'
         self.l_number = 0
-        self.static_map_params = {'l': 'map', 'll': f'{str(self.ll[0])},{str(self.ll[1])}',
+        self.static_map_params = {'l': self.current_visibility,
+                                  'll': f'{str(self.ll[0])},{str(self.ll[1])}',
                                   'size': '640,450', 'z': str(self.z)}
         self.geocoder_link = 'https://static-maps.yandex.ru/1.x'
         self.geocoder_params = {'apikey': '40d1649f-0493-4b70-98ba-98533de7710b', 'gecode': None,
@@ -23,13 +24,27 @@ class Example(Ui_MainWindow, QMainWindow):
         self.image = None
         self.pixmap = None
         self.response = None
+        self.map_radio.clicked.connect(self.change_visibility)
+        self.sat_radio.clicked.connect(self.change_visibility)
+        self.skl_radio.clicked.connect(self.change_visibility)
         self.static_map_request()
         self.set_pixmap()
+
+    def change_visibility(self):
+        if self.map_radio.isChecked():
+            self.current_visibility = 'map'
+        elif self.sat_radio.isChecked():
+            self.current_visibility = 'sat'
+        elif self.skl_radio.isChecked():
+            self.current_visibility = 'sat,skl'
+        self.static_map_params['l'] = self.current_visibility
+        self.static_map_request()
 
     def static_map_request(self):
         self.image = requests.get(self.static_map_link, self.static_map_params).content
         with open('map.png', 'wb') as file:
             file.write(self.image)
+        self.set_pixmap()
 
     def set_pixmap(self):
         self.pixmap = QPixmap('map.png')
@@ -58,7 +73,6 @@ class Example(Ui_MainWindow, QMainWindow):
         self.static_map_params['ll'] = f'{str(self.ll[0])},{str(self.ll[1])}'
         self.static_map_params['z'] = str(self.z)
         self.static_map_request()
-        self.set_pixmap()
 
 
 if __name__ == '__main__':
