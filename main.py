1df4f2c4716b97575947f8822f0b3582c76f2a23
Mr Zoom <mr.zoom.orsk@gmail.com>
2022-02-04 17:27:48 +0500

    Added Z

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtGui import QPixmap
 from interface import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
 
 
 class Example(Ui_MainWindow, QMainWindow):
@@ -10,7 +11,10 @@ class Example(Ui_MainWindow, QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.static_map_link = 'https://static-maps.yandex.ru/1.x'
-        self.static_map_params = {'l': 'map', 'll': '37.622513,55.753220', 'size': '640,450', 'z': '10'}
+        self.z = 10
+        self.l_list = ['map', 'sat', 'skl']
+        self.l_number = 0
+        self.static_map_params = {'l': 'map', 'll': '37.622513,55.753220', 'size': '640,450', 'z': str(self.z)}
         self.geocoder_link = 'https://static-maps.yandex.ru/1.x'
         self.geocoder_params = {'apikey': '40d1649f-0493-4b70-98ba-98533de7710b', 'gecode': None,
                                 'format': 'json'}
@@ -31,6 +35,19 @@ class Example(Ui_MainWindow, QMainWindow):
 
     def geocoder_request(self):
         self.response = requests.get(self.geocoder_link, self.geocoder_params)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.z += 1
+            if self.z > 17:
+                self.z = 17
+        if event.key() == Qt.Key_PageDown:
+            self.z -= 1
+            if self.z < 1:
+                self.z = 1
+        self.static_map_params = {'l': 'map', 'll': '37.622513,55.753220', 'size': '640,450', 'z': str(self.z)}
+        self.static_map_request()
+        self.set_pixmap()
 
 
 if __name__ == '__main__':
